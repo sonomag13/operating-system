8417b5c9a7735a7ca4b2aef31335bd6241484b02
Lu Gao <sonomag13@gmail.com>
2021-01-10 22:56:28 -0800

    join and detach implemented

--- join-and-detach/main.cpp
+++ join-and-detach/main.cpp
@@ -12,13 +12,14 @@ void run(int count) {
         char str[1024];
         sprintf(str, "count = %d", count);
         cout << str << endl;
+        this_thread::sleep_for(0.1s);
     }
     cout << "the thread job ends" << endl;
 }
 
 int main() {
 
-    int num{10};
+    int num{5};
 
     // the case where the thread will join
     cout << "join section starts" << endl;
@@ -38,13 +39,16 @@ int main() {
         cout << "join the thread" << endl;
     }
     /**
-     * if we ask the main thread to sleep for 0.1 second,
+     * if we ask the main thread to sleep for 1 second,
      * there will be sufficient time to complete run(...)
+     * In case the sleeping time is 0.2sec, the run will
+     * print approximately 2 times
      *
-     * Otherwise, it is very likely that the main thread ends
-     * before t2 complete running run(...)
+     * Without the sleeping time in the main thread, it is
+     * very likely that the main thread ends before t2
+     * complete running run(...)
      **/
-    this_thread::sleep_for(0.1s);
+    this_thread::sleep_for(1s);
     cout << "detach section ends" << endl;
 
     return 0;
