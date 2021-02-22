7ff22c0879079d1538f3a8c872d0cf3f795d1cb5
Lu Gao <lgao@zoox.com>
2021-02-21 23:18:13 -0800

    figured out how to pass in the result

--- thread-pool/main.py
+++ thread-pool/main.py
@@ -8,27 +8,61 @@ import random
 import time
 
 
-def work(info):
-    print("i = {}".format(info["idx"]))
+def worker_1(*args):
+    """
+    :param args: there should be 2 arguments
+                - 1st argument: the dictionary where we insert the result (key: index; val: sleep time)
+                - 2nd argument: index
+    :return: n/a
+    """
     val = random.randint(1, 5)
+    print("[worker 1] i = {} and val = {}".format(args[1], val))
     time.sleep(val)
-    info["dict"].update({info["idx"]: val})
+    args[0].update({args[1]: val})
+
+
+def worker_2(**kwargs):
+    """
+    :param kwargs: there should be 2 arguments:
+                - key "idx" points to the current index
+                - key "working_dict" points to the dictionary where we insert the result (key: index; val: sleep time)
+    :return: n/a
+    """
+    val = random.randint(1, 8)
+    print("[worker 2] i = {} and val = {}".format(kwargs["idx"], val))
+    time.sleep(val)
+    kwargs["working_dict"].update({kwargs["idx"]: val})
+
+
+def print_dictionary(input_dict, idx):
+    """
+    :param input_dict: a dictionary to print
+    :param idx: the index of the dictionary
+    :return: n/a
+    """
+    print("\n\nworking dict {}".format(idx))
+    for key, val in input_dict.items():
+        print("\tkey = {} and val = {}".format(key, val))
 
 
 def main():
-    work_dict = {}
+
+    # dictionaries are passed by reference
+    working_dict_1 = {}
+    working_dict_2 = {}
+
+    # initialize the pool of threads
     pool = ThreadPoolExecutor(max_workers=3)
     num = 20
     for i in range(num):
-        info = {"dict": work_dict, "idx": i}
-        pool.submit(work, info)
+        pool.submit(worker_1, working_dict_1, i)
+        pool.submit(worker_2, working_dict=working_dict_2, idx=i)
 
     # shutdonw ensures all the task in the pool to be completed
     pool.shutdown(wait=True)
 
-    # interestingly the dictionary is passed by the reference
-    for key in work_dict:
-        print("key = {} and val = {}".format(key, work_dict[key]))
+    print_dictionary(working_dict_1, 1)
+    print_dictionary(working_dict_2, 2)
 
 
 if __name__ == '__main__':
